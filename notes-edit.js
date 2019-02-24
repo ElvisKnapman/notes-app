080c2ff967cfe783016c5987db927822c3fe32e0
Elvis Knapman <elvis@elvisknapman.com>
2019-02-24 13:10:50 -0500

    Implement initial functionality for note editing
    Title has an event listener that updates and saves with every input, and the body element has the same. Remove note button when clicked deletes the note from the notes array and returns the user to the main page.

--- notes-edit.js
+++ notes-edit.js
@@ -1,12 +1,41 @@
+const titleElement = document.querySelector('#note-title');
+const bodyElement = document.querySelector('#note-body');
+const removeElement = document.querySelector('#remove-note');
+
+// extract note id from URL hash and remove the '#' from the string
 const noteId = location.hash.substring(1);
+
+// retrieve notes stored in localStorage
 const notes = getSavedNotes();
+
+// find and store the current note object being edited
 const note = notes.find((note) => {
     return note.id === noteId;
 });
 
+// if a note isn't found, redirect back to the home page
 if (note === undefined) {
     location.assign('/index.html');
 }
 
-document.querySelector('#note-title').value = note.title;
-document.querySelector('#note-body').value = note.body;
+titleElement.value = note.title;
+bodyElement.value = note.body;
+
+titleElement.addEventListener('input', (event) => {
+    // update note title as user types and save 
+    note.title = event.target.value;
+    saveNotes(notes);
+});
+
+bodyElement.addEventListener('input', (event) => {
+    // update note body as user types and save 
+    note.body = event.target.value;
+    saveNotes(notes);
+});
+
+removeElement.addEventListener('click', (event) => {
+    // remove note, save notes and redirect back to home page
+    removeNote(note.id);
+    saveNotes(notes);
+    location.assign('/index.html');
+});
